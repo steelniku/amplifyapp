f0d279acaaacaadc159437889a9cd4d117f747fa
Nikke <nikke@MacNikke.local>
2021-01-22 17:28:48 +0200

    sec mod

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br>
-	F'in jei!	
+	Freaking jei!	
         </p>
         <a
           className="App-link"
